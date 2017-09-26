e566839b4b5c222e3d7755df03b3cbe76b771340
zyl20131322 <625433779@qq.com>
2017-09-26 16:41:14 +0800

    first commit

---
+++ js/baidu.js
@@ -0,0 +1,11 @@
+// 展开更多产品
+function displayMoreProduct() {
+        document.getElementById("hidemp").style.display = "none";
+        document.getElementById("moreproduct").style.display = "block";
+}
+//隐藏更多产品
+function hideMoreProduct() {
+
+    document.getElementById("moreproduct").style.display = "none";
+    document.getElementById("hidemp").style.display = "block";
+}
